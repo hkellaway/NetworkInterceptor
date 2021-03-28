ac7f634f35badc0e01d6a75cfd166610ef0736e9
Harlan Kellaway <hkellaway@users.noreply.github.com>
2021-03-27 20:17:07 -0400

    Create new framework project

---
+++ Nog/Nog/Nog.h
@@ -0,0 +1,18 @@
+//
+//  Nog.h
+//  Nog
+//
+//  Created by Harlan Kellaway on 3/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Nog.
+FOUNDATION_EXPORT double NogVersionNumber;
+
+//! Project version string for Nog.
+FOUNDATION_EXPORT const unsigned char NogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nog/PublicHeader.h>
+
+
---
+++ Nog/NogTests/NogTests.swift
@@ -0,0 +1,33 @@
+//
+//  NogTests.swift
+//  NogTests
+//
+//  Created by Harlan Kellaway on 3/27/21.
+//
+
+import XCTest
+@testable import Nog
+
+class NogTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
