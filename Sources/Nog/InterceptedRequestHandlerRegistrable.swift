cf6b3cf742ff040c62741da538a8e5c448f176f9
Harlan Kellaway <hkellaway@users.noreply.github.com>
2021-03-27 22:55:26 -0400

    Remove Slack request handling

--- Sources/Nog/InterceptedRequestHandlerRegistrable.swift
+++ Sources/Nog/InterceptedRequestHandlerRegistrable.swift
@@ -10,14 +10,11 @@ import Foundation
 
 public enum SniffableRequestHandlerRegistrable {
     case console(logginMode: ConsoleLoggingMode)
-    case slack(slackToken: String, channel: String, username: String)
     
     public func requestHandler() -> SniffableRequestHandler {
         switch self {
         case .console(let loggingMode):
             return ConsoleLoggerSniffableRequestHandler(loggingMode: loggingMode)
-        case .slack(let slackToken, let channel, let username):
-            return SlackSniffableRequestHandler(slackToken: slackToken, channel: channel, username: username)
         }
     }
 }
