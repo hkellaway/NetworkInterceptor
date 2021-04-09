890f698b0fd4782a86c7522cd54b42039f5b7bb0
Harlan Kellaway <hkellaway@users.noreply.github.com>
2021-04-09 19:18:56 -0400

    Pare down Alamofire cURL helpers to necessary

--- Sources/Nog/cURL.swift
+++ Sources/Nog/cURL.swift
@@ -81,22 +81,6 @@ public struct HTTPHeaders {
     /// - Parameters:
     ///   - name:  The `HTTPHeader` name.
     ///   - value: The `HTTPHeader value.
-    public mutating func add(name: String, value: String) {
-        update(HTTPHeader(name: name, value: value))
-    }
-
-    /// Case-insensitively updates or appends the provided `HTTPHeader` into the instance.
-    ///
-    /// - Parameter header: The `HTTPHeader` to update or append.
-    public mutating func add(_ header: HTTPHeader) {
-        update(header)
-    }
-
-    /// Case-insensitively updates or appends an `HTTPHeader` into the instance using the provided `name` and `value`.
-    ///
-    /// - Parameters:
-    ///   - name:  The `HTTPHeader` name.
-    ///   - value: The `HTTPHeader value.
     public mutating func update(name: String, value: String) {
         update(HTTPHeader(name: name, value: value))
     }
@@ -120,21 +104,6 @@ public struct HTTPHeaders {
         guard let index = headers.index(of: name) else { return }
 
         headers.remove(at: index)
-    }
-
-    /// Sort the current instance by header name, case insensitively.
-    public mutating func sort() {
-        headers.sort { $0.name.lowercased() < $1.name.lowercased() }
-    }
-
-    /// Returns an instance sorted by header name.
-    ///
-    /// - Returns: A copy of the current instance sorted by name.
-    public func sorted() -> HTTPHeaders {
-        var headers = self
-        headers.sort()
-
-        return headers
     }
 
     /// Case-insensitively find a header's value by name.
@@ -180,26 +149,6 @@ extension Array where Element == HTTPHeader {
     }
 }
 
-extension HTTPHeaders: ExpressibleByDictionaryLiteral {
-    public init(dictionaryLiteral elements: (String, String)...) {
-        self.init()
-
-        elements.forEach { update(name: $0.0, value: $0.1) }
-    }
-}
-
-extension HTTPHeaders: ExpressibleByArrayLiteral {
-    public init(arrayLiteral elements: HTTPHeader...) {
-        self.init(elements)
-    }
-}
-
-extension HTTPHeaders: Sequence {
-    public func makeIterator() -> IndexingIterator<[HTTPHeader]> {
-        headers.makeIterator()
-    }
-}
-
 extension HTTPHeaders: Collection {
     public var startIndex: Int {
         headers.startIndex
@@ -215,13 +164,6 @@ extension HTTPHeaders: Collection {
 
     public func index(after i: Int) -> Int {
         headers.index(after: i)
-    }
-}
-
-extension HTTPHeaders: CustomStringConvertible {
-    public var description: String {
-        headers.map { $0.description }
-            .joined(separator: "\n")
     }
 }
 
@@ -241,119 +183,6 @@ public struct HTTPHeader: Hashable {
     public init(name: String, value: String) {
         self.name = name
         self.value = value
-    }
-}
-
-extension HTTPHeader: CustomStringConvertible {
-    public var description: String {
-        "\(name): \(value)"
-    }
-}
-
-extension HTTPHeader {
-    /// Returns an `Accept` header.
-    ///
-    /// - Parameter value: The `Accept` value.
-    /// - Returns:         The header.
-    public static func accept(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Accept", value: value)
-    }
-
-    /// Returns an `Accept-Charset` header.
-    ///
-    /// - Parameter value: The `Accept-Charset` value.
-    /// - Returns:         The header.
-    public static func acceptCharset(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Accept-Charset", value: value)
-    }
-
-    /// Returns an `Accept-Language` header.
-    ///
-    /// Alamofire offers a default Accept-Language header that accumulates and encodes the system's preferred languages.
-    /// Use `HTTPHeader.defaultAcceptLanguage`.
-    ///
-    /// - Parameter value: The `Accept-Language` value.
-    ///
-    /// - Returns:         The header.
-    public static func acceptLanguage(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Accept-Language", value: value)
-    }
-
-    /// Returns an `Accept-Encoding` header.
-    ///
-    /// Alamofire offers a default accept encoding value that provides the most common values. Use
-    /// `HTTPHeader.defaultAcceptEncoding`.
-    ///
-    /// - Parameter value: The `Accept-Encoding` value.
-    ///
-    /// - Returns:         The header
-    public static func acceptEncoding(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Accept-Encoding", value: value)
-    }
-
-    /// Returns a `Basic` `Authorization` header using the `username` and `password` provided.
-    ///
-    /// - Parameters:
-    ///   - username: The username of the header.
-    ///   - password: The password of the header.
-    ///
-    /// - Returns:    The header.
-    public static func authorization(username: String, password: String) -> HTTPHeader {
-        let credential = Data("\(username):\(password)".utf8).base64EncodedString()
-
-        return authorization("Basic \(credential)")
-    }
-
-    /// Returns a `Bearer` `Authorization` header using the `bearerToken` provided
-    ///
-    /// - Parameter bearerToken: The bearer token.
-    ///
-    /// - Returns:               The header.
-    public static func authorization(bearerToken: String) -> HTTPHeader {
-        authorization("Bearer \(bearerToken)")
-    }
-
-    /// Returns an `Authorization` header.
-    ///
-    /// Alamofire provides built-in methods to produce `Authorization` headers. For a Basic `Authorization` header use
-    /// `HTTPHeader.authorization(username:password:)`. For a Bearer `Authorization` header, use
-    /// `HTTPHeader.authorization(bearerToken:)`.
-    ///
-    /// - Parameter value: The `Authorization` value.
-    ///
-    /// - Returns:         The header.
-    public static func authorization(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Authorization", value: value)
-    }
-
-    /// Returns a `Content-Disposition` header.
-    ///
-    /// - Parameter value: The `Content-Disposition` value.
-    ///
-    /// - Returns:         The header.
-    public static func contentDisposition(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Content-Disposition", value: value)
-    }
-
-    /// Returns a `Content-Type` header.
-    ///
-    /// All Alamofire `ParameterEncoding`s and `ParameterEncoder`s set the `Content-Type` of the request, so it may not be necessary to manually
-    /// set this value.
-    ///
-    /// - Parameter value: The `Content-Type` value.
-    ///
-    /// - Returns:         The header.
-    public static func contentType(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "Content-Type", value: value)
-    }
-
-    /// Returns a `User-Agent` header.
-    ///
-    /// - Parameter value: The `User-Agent` value.
-    ///
-    /// - Returns:         The header.
-    public static func userAgent(_ value: String) -> HTTPHeader {
-        HTTPHeader(name: "User-Agent", value: value)
     }
 }
 
